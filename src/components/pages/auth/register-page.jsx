de2ecdf776cdab72be26b73233538927c1e15ec4
m-istighfar <istighfar.amal@gmail.com>
2023-12-15 09:34:14 +0700

    improve register with fetch api

--- src/components/pages/auth/register-page.jsx
+++ src/components/pages/auth/register-page.jsx
@@ -1,4 +1,77 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { Dialog, Listbox } from "@headlessui/react";
+import { useNavigate } from "react-router-dom";
+
 const RegisterPage = () => {
+  // State untuk menyimpan daftar provinsi dan provinsi yang dipilih
+  const [provinces, setProvinces] = useState([]);
+  const [selectedProvince, setSelectedProvince] = useState(null);
+
+  // State untuk mengatur modal (buka/tutup dan jenisnya)
+  const [modalState, setModalState] = useState({ isOpen: false, type: "" });
+
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  // useEffect untuk mengambil data provinsi dari API
+  useEffect(() => {
+    const fetchProvinces = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/province");
+        setProvinces(response.data.data);
+        setSelectedProvince(response.data.data[0]);
+      } catch (error) {
+        console.error("Error fetching provinces:", error);
+      }
+    };
+
+    fetchProvinces();
+  }, []);
+
+  // useEffect untuk mengatur nilai 'provinceId' pada form berdasarkan provinsi yang dipilih
+  useEffect(() => {
+    setValue("provinceId", selectedProvince?.ProvinceID);
+  }, [selectedProvince, setValue]);
+
+  // Fungsi yang dijalankan ketika form disubmit
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/auth/register",
+        data,
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      console.log(response.data);
+      setModalState({ isOpen: true, type: "success" });
+      reset();
+    } catch (error) {
+      console.error("Error during registration:", error);
+      setModalState({
+        isOpen: true,
+        type: "error",
+        message: error.response?.data?.error || "Registration failed",
+      });
+    }
+  };
+
+  // Fungsi untuk menutup modal
+  const closeModal = () => {
+    setModalState({ isOpen: false, type: "" });
+    if (modalState.type === "success") {
+      navigate("/login");
+    }
+  };
+
   return (
     <main className="bg-off_white min-h-screen flex items-center justify-center p-8 md:p-0">
       <div className="bg-white shadow-lg flex flex-col items-center rounded-rmd overflow-hidden lg:flex-row lg:w-2/3 2xl:w-1/2">
@@ -7,17 +80,22 @@ const RegisterPage = () => {
             Join us in saving lives.
           </h1>
 
-          <form className="flex flex-col space-y-4 mt-8">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col space-y-4 mt-8"
+          >
             <div>
               <label htmlFor="username" className="block mb-2 text-dark_gray">
                 Username
               </label>
               <input
                 id="username"
-                type="text"
-                placeholder="Choose a username"
+                {...register("username", { required: "Username is required" })}
                 className="w-full px-4 py-2 border-2 border-gray rounded-rsm focus:border-dark_red focus:outline-none"
               />
+              {errors.username && (
+                <p className="text-red-500">{errors.username.message}</p>
+              )}
             </div>
 
             <div>
@@ -26,10 +104,18 @@ const RegisterPage = () => {
               </label>
               <input
                 id="email"
-                type="email"
-                placeholder="youremail@example.com"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: /^\S+@\S+\.\S+$/,
+                    message: "Invalid email format",
+                  },
+                })}
                 className="w-full px-4 py-2 border-2 border-gray rounded-rsm focus:border-dark_red focus:outline-none"
               />
+              {errors.email && (
+                <p className="text-red-500">{errors.email.message}</p>
+              )}
             </div>
 
             <div>
@@ -38,10 +124,13 @@ const RegisterPage = () => {
               </label>
               <input
                 id="password"
+                {...register("password", { required: "Password is required" })}
                 type="password"
-                placeholder="Create a password"
                 className="w-full px-4 py-2 border-2 border-gray rounded-rsm focus:border-dark_red focus:outline-none"
               />
+              {errors.password && (
+                <p className="text-red-500">{errors.password.message}</p>
+              )}
             </div>
 
             <div>
@@ -50,10 +139,12 @@ const RegisterPage = () => {
               </label>
               <input
                 id="name"
-                type="text"
-                placeholder="John Doe"
+                {...register("name", { required: "Full Name is required" })}
                 className="w-full px-4 py-2 border-2 border-gray rounded-rsm focus:border-dark_red focus:outline-none"
               />
+              {errors.name && (
+                <p className="text-red-500">{errors.name.message}</p>
+              )}
             </div>
 
             <div>
@@ -62,11 +153,50 @@ const RegisterPage = () => {
               </label>
               <input
                 id="phone"
+                {...register("phone", { required: "Phone number is required" })}
                 type="tel"
-                placeholder="+1234567890"
                 className="w-full px-4 py-2 border-2 border-gray rounded-rsm focus:border-dark_red focus:outline-none"
               />
+              {errors.phone && (
+                <p className="text-red-500">{errors.phone.message}</p>
+              )}
             </div>
+
+            <Listbox value={selectedProvince} onChange={setSelectedProvince}>
+              {({ open }) => (
+                <>
+                  <Listbox.Label className="block mb-2 text-dark_gray">
+                    Province
+                  </Listbox.Label>
+                  <Listbox.Button className="w-full px-4 py-2 border-2 border-gray rounded-rsm focus:border-dark_red focus:outline-none">
+                    {selectedProvince?.Name || "Select a province"}
+                  </Listbox.Button>
+                  {open && (
+                    <Listbox.Options className="w-full mt-1 max-h-60 border-2 border-gray rounded-rsm overflow-y-auto focus:outline-none">
+                      {provinces.map((province) => (
+                        <Listbox.Option
+                          key={province.ProvinceID}
+                          value={province}
+                          as={React.Fragment}
+                        >
+                          {({ active }) => (
+                            <li
+                              className={`${
+                                active
+                                  ? "bg-blue-500 text-white"
+                                  : "text-gray-900"
+                              } cursor-pointer select-none relative px-4 py-2`}
+                            >
+                              {province.Name}
+                            </li>
+                          )}
+                        </Listbox.Option>
+                      ))}
+                    </Listbox.Options>
+                  )}
+                </>
+              )}
+            </Listbox>
 
             <button
               type="submit"
@@ -92,6 +222,35 @@ const RegisterPage = () => {
           />
         </div>
       </div>
+
+      {/* Modal untuk menampilkan pesan setelah submit */}
+      <Dialog
+        open={modalState.isOpen}
+        onClose={closeModal}
+        className="relative z-50"
+      >
+        <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+        <div className="fixed inset-0 flex items-center justify-center p-4">
+          <Dialog.Panel className="w-full max-w-md rounded bg-white p-6">
+            <Dialog.Title className="text-lg font-medium leading-6 text-gray-900">
+              {modalState.type === "success"
+                ? "Registration Successful"
+                : "Error"}
+            </Dialog.Title>
+            <Dialog.Description className="mt-2">
+              {modalState.type === "success"
+                ? "Congratulations! Your account has been successfully created. You can now log in with your credentials."
+                : modalState.message}
+            </Dialog.Description>
+            <button
+              onClick={closeModal}
+              className="mt-4 bg-dark_red hover:bg-red text-white font-medium text-lg px-4 py-2 rounded-rsm transition-colors duration-200"
+            >
+              OK
+            </button>
+          </Dialog.Panel>
+        </div>
+      </Dialog>
     </main>
   );
 };
