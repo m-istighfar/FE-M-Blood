00ec2d6b46d5d8806a26494bde8c249d41f8e878
m-istighfar <istighfar.amal@gmail.com>
2023-12-19 14:24:03 +0700

    handile submit - donate blood

--- src/components/pages/donate-blood/donate-blood-page.jsx
+++ src/components/pages/donate-blood/donate-blood-page.jsx
@@ -10,180 +10,210 @@ import BeforeFooterCTA from "../../sections/before-footer-cta/before-footer-cta-
 import FooterComponent from "../../sections/footer/footer-component";
 import { useForm } from "react-hook-form";
 
-
-import newUsersInsertRequest from "../../utility-functions/new-users-insert-request";
-
 import axios from "axios";
 
 const DonateBloodPage = () => {
-	const [bloodTypes, setBloodTypes] = useState([]);
-	const [selectedBlood, setSelectedBlood] = useState(null);
-	const { setValue } = useForm();
+  const [bloodTypes, setBloodTypes] = useState([]);
+  const [provinces, setProvinces] = useState([]);
+  const [formStatus, setFormStatus] = useState({ loading: false, message: "" });
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
-	const [formData, setFormData] = useState({
-		bloodType: "",
-		scheduledDate: "",
-	});
+  useEffect(() => {
+    const fetchBloodTypes = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/blood-type");
+        setBloodTypes(response.data.data);
+      } catch (error) {
+        console.error("Error fetching blood types:", error);
+      }
+    };
+    const fetchProvinces = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/province");
+        setProvinces(response.data.data);
+      } catch (error) {
+        console.error("Error fetching provinces:", error);
+      }
+    };
 
-	useEffect(() => {
-		const fetchBlood = async () => {
-			try {
-				const response = await axios.get("http://localhost:3000/blood-type");
-				setBloodTypes(response.data.data);
-				setSelectedBlood(response.data.data[0]);
-			} catch (error) {
-				console.error("Error fetching blood:", error);
-			}
-		};
-		
-			fetchBlood();
-		}, []);
+    fetchBloodTypes();
+    fetchProvinces();
+  }, []);
 
-		useEffect(() => {
-			setValue("BloodTypeID", selectedBlood?.BloodTypeID);
-		}, [selectedBlood, setValue]);
+  const onSubmit = async (data) => {
+    setFormStatus({ loading: true, message: "" });
+    const accessToken = localStorage.getItem("accessToken");
+    if (!accessToken) {
+      console.error("No access token found. User must be logged in.");
+      setFormStatus({ loading: false, message: "User must be logged in." });
+      return;
+    }
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/appointments/create",
+        data,
+        { headers: { Authorization: `Bearer ${accessToken}` } }
+      );
+      console.log("Appointment created successfully:", response.data);
+      reset();
+      setFormStatus({
+        loading: false,
+        message:
+          "Appointment created successfully. Thank you for your willingness to donate blood!",
+      });
+    } catch (error) {
+      console.error("Error creating appointment:", error);
 
-		console.log("success");
+      let errorMessage = "Error creating appointment.";
+      if (error.response && error.response.data && error.response.data.error) {
+        errorMessage = error.response.data.error;
+      }
 
-		axios.post("http://localhost:3000/appointments/create", {
-			bloodType: formData.bloodType,
-			scheduledDate: formData.scheduledDate,
-		})
-			.then((response) => {
-				console.log("success");
-				console.log(response.data);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
+      setFormStatus({
+        loading: false,
+        message: errorMessage,
+      });
+    }
+  };
 
-		newUsersInsertRequest(formData, "donate-blood");
-
-		setFormData({
-			bloodType: "",
-			scheduledDate: "",
-		});
-	};
-
-	const DonateBloodPageDetails = {
-		quote: {
-			classHint: "quote",
-			quoteText: `“By donating money, you provide nourishment. By donating blood, you give the gift of life. Join us in this noble cause today!”`,
-		},
-		why_donate_blood: {
-			subheadingText: "Donate blood today",
-			headingText: "Why should you donate blood?",
-			classHint: "side-col-image why-donate-blood",
-			paraText: `Donating blood is a selfless act that has the power to save lives. Here are a few reasons why you should consider donating blood:
+  const DonateBloodPageDetails = {
+    quote: {
+      classHint: "quote",
+      quoteText: `“By donating money, you provide nourishment. By donating blood, you give the gift of life. Join us in this noble cause today!”`,
+    },
+    why_donate_blood: {
+      subheadingText: "Donate blood today",
+      headingText: "Why should you donate blood?",
+      classHint: "side-col-image why-donate-blood",
+      paraText: `Donating blood is a selfless act that has the power to save lives. Here are a few reasons why you should consider donating blood:
 			\n― You could help save up to three lives with just one donation.
 			― Blood is always needed in emergency situations, such as natural disasters and accidents.
 			― Blood is needed for patients undergoing surgeries, cancer treatment, and other medical procedures.
 			― Blood cannot be manufactured, which means that the only source of blood is through donations from volunteers.
 			― Donating blood can also have health benefits for the donor, such as reducing the risk of heart disease and cancer.`,
-			imageUrl: "../../../assets/images/blood-donation(1).jpg",
-		},
-		eligiblity_criteria: {
-			subheadingText: "Are you ready?",
-			headingText: "Eligibility Criteria",
-			classHint: "side-col-image eligibility-criteria",
-			paraText: [
-				`18-50 years, above 50 Kg.`,
-				`Normal temperature, pulse and blood pressure.`,
-				`No Respiratory Diseases`,
-				`Above 12.5 g/dL Hemoglobin`,
-				`No skin disease, puncture or scars`,
-				`No history of transmissible disease`,
-			],
-			imageUrl: "../../../assets/images/blood-donation(1).jpg",
-			buttonText: "Donate Now",
-			buttonLink: "/donate-blood",
-			buttonHave: false,
-		},
-		hero: {
-			subheadingText: "Donate Blood",
-			headingText: "Save Life By Donating Blood Today",
-			classHint: "donate-blood-page-hero",
-		},
-		stepsText: {
-			subheadingText: "Donation Process",
-			headingText: "Step-by-Step Guide to Donating Blood",
-		},
-	};
+      imageUrl: "../../../assets/images/blood-donation(1).jpg",
+    },
+    eligiblity_criteria: {
+      subheadingText: "Are you ready?",
+      headingText: "Eligibility Criteria",
+      classHint: "side-col-image eligibility-criteria",
+      paraText: [
+        `18-50 years, above 50 Kg.`,
+        `Normal temperature, pulse and blood pressure.`,
+        `No Respiratory Diseases`,
+        `Above 12.5 g/dL Hemoglobin`,
+        `No skin disease, puncture or scars`,
+        `No history of transmissible disease`,
+      ],
+      imageUrl: "../../../assets/images/blood-donation(1).jpg",
+      buttonText: "Donate Now",
+      buttonLink: "/donate-blood",
+      buttonHave: false,
+    },
+    hero: {
+      subheadingText: "Donate Blood",
+      headingText: "Save Life By Donating Blood Today",
+      classHint: "donate-blood-page-hero",
+    },
+    stepsText: {
+      subheadingText: "Donation Process",
+      headingText: "Step-by-Step Guide to Donating Blood",
+    },
+  };
 
-	const stepDetails = [
-		{
-			key: "check-eligibility",
-			stepNumber: "01",
-			stepName: "Check your eligibility",
-			stepDescription:
-				"Confirm you meet the eligibility requirements to donate blood, such as age, weight, and overall health.",
-		},
-		{
-			key: "schedule-an-appointment",
-			stepNumber: "02",
-			stepName: "Schedule an appointment",
-			stepDescription:
-				"Schedule an appointment at a blood bank or blood drive near you.",
-		},
-		{
-			key: "donate-blood",
-			stepNumber: "03",
-			stepName: "Donate Blood",
-			stepDescription:
-				"Arrive at the appointment, fill out a questionnaire, and donate blood. The process takes about 10-15 minutes.",
-		},
-	];
+  const stepDetails = [
+    {
+      key: "check-eligibility",
+      stepNumber: "01",
+      stepName: "Check your eligibility",
+      stepDescription:
+        "Confirm you meet the eligibility requirements to donate blood, such as age, weight, and overall health.",
+    },
+    {
+      key: "schedule-an-appointment",
+      stepNumber: "02",
+      stepName: "Schedule an appointment",
+      stepDescription:
+        "Schedule an appointment at a blood bank or blood drive near you.",
+    },
+    {
+      key: "donate-blood",
+      stepNumber: "03",
+      stepName: "Donate Blood",
+      stepDescription:
+        "Arrive at the appointment, fill out a questionnaire, and donate blood. The process takes about 10-15 minutes.",
+    },
+  ];
 
-	const fields = [
-		{
-			key: "bloodType",
-			name: "bloodType",
-			type: "select",
-			options: bloodTypes.map((bloodType) => ({
-			value: bloodType.BloodID,
-			label: bloodType.Type,
-			})),
-			placeholder: "Blood Type",
-			required: true,
-		},
-		{
-			key: "scheduledDate",
-			name: "scheduledDate",
-			type: "date", 
-			placeholder: "Scheduled Date",
-			required: true,
-		},
-	];
+  const fields = [
+    {
+      key: "bloodType",
+      name: "bloodType",
+      type: "select",
+      options: [
+        { value: "", label: "Select Blood Type", disabled: true },
+        ...bloodTypes.map((type) => ({ value: type.Type, label: type.Type })),
+      ],
+      placeholder: "Blood Type",
+      required: "Blood type is required",
+    },
+    {
+      key: "location",
+      name: "location",
+      type: "select",
+      options: [
+        { value: "", label: "Select a Province", disabled: true },
+        ...provinces.map((province) => ({
+          value: province.Name,
+          label: province.Name,
+        })),
+      ],
+      placeholder: "Location",
+      required: "Location is required",
+    },
+    {
+      key: "scheduledDate",
+      name: "scheduledDate",
+      type: "date",
+      placeholder: "Scheduled Date",
+      required: true,
+    },
+  ];
 
-	return (
-		<>
-			<HeaderComponent />
+  return (
+    <>
+      <HeaderComponent />
 
-			<HeroComponent {...DonateBloodPageDetails.hero} />
-			<FormComponent
-				fields={fields}
-				heading={"Schedule an Appointment"}
-				buttonText={"Schedule an Appointment"}
-				handleSubmit={handleSubmit}
-				formData={formData}
-				setFormData={setFormData}
-			/>
-			<ThreeStepProcessComponent
-				stepsText={DonateBloodPageDetails.stepsText}
-				stepDetails={stepDetails}
-			/>
-			<CriteriaComponent
-				{...DonateBloodPageDetails.eligiblity_criteria}
-			/>
-			<SideBySideComponent {...DonateBloodPageDetails.why_donate_blood} />
-			<QuoteComponent {...DonateBloodPageDetails.quote} />
-			<BeforeFooterCTA />
-			<FooterComponent />
-		</>
-	);
+      <HeroComponent {...DonateBloodPageDetails.hero} />
+      <FormComponent
+        isLoading={formStatus.loading}
+        fields={fields}
+        register={register}
+        handleSubmit={handleSubmit(onSubmit)}
+        errors={errors}
+        heading={"Schedule an Appointment"}
+        buttonText={"Schedule Now"}
+      />
+      {formStatus.message && (
+        <p className="error-message text-center">{formStatus.message}</p>
+      )}
+      <ThreeStepProcessComponent
+        stepsText={DonateBloodPageDetails.stepsText}
+        stepDetails={stepDetails}
+      />
+      <CriteriaComponent {...DonateBloodPageDetails.eligiblity_criteria} />
+      <SideBySideComponent {...DonateBloodPageDetails.why_donate_blood} />
+      <QuoteComponent {...DonateBloodPageDetails.quote} />
+      <BeforeFooterCTA />
+      <FooterComponent />
+    </>
+  );
 };
 
 export default DonateBloodPage;
--- src/components/pages/host-blood-drive/host-blood-drive.jsx
+++ src/components/pages/host-blood-drive/host-blood-drive.jsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from "react";
 import HeroComponent from "../../sections/hero/hero-component";
 import ThreeStepProcessComponent from "../../sections/three-step-process/three-step-process-component";
 import SideBySideComponent from "../../sections/side-by-side/side-by-side-component";
@@ -9,193 +8,188 @@ import BeforeFooterCTA from "../../sections/before-footer-cta/before-footer-cta-
 import FooterComponent from "../../sections/footer/footer-component";
 import { useForm } from "react-hook-form";
 
-
 import axios from "axios";
 import newUsersInsertRequest from "../../utility-functions/new-users-insert-request";
 
 const HostBloodDrivePage = () => {
+  const [provinces, setProvinces] = useState([]);
+  const [selectedProvince, setSelectedProvince] = useState(null);
 
-	const [provinces, setProvinces] = useState([]);
-	const [selectedProvince, setSelectedProvince] = useState(null);
+  const { setValue } = useForm();
 
-	const { setValue } = useForm();
+  const [formData, setFormData] = useState({
+    institute: "",
+    province: "",
+    designation: "",
+    schedule: "",
+  });
 
-	const [formData, setFormData] = useState({
-		institute: "",
-		province:"",
-		designation: "",
-		schedule:""
-	});
+  useEffect(() => {
+    const fetchProvinces = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/province");
+        setProvinces(response.data.data);
+        setSelectedProvince(response.data.data[0]);
+      } catch (error) {
+        console.error("Error fetching provinces:", error);
+      }
+    };
 
-	useEffect(() => {
-		const fetchProvinces = async () => {
-			try {
-				const response = await axios.get("http://localhost:3000/province");
-				setProvinces(response.data.data);
-				setSelectedProvince(response.data.data[0]);
-			} catch (error) {
-				console.error("Error fetching provinces:", error);
-			}
-		};
-		
-			fetchProvinces();
-		}, []);
+    fetchProvinces();
+  }, []);
 
-		useEffect(() => {
-			setValue("provinceId", selectedProvince?.ProvinceID);
-		}, [selectedProvince, setValue]);
+  useEffect(() => {
+    setValue("provinceId", selectedProvince?.ProvinceID);
+  }, [selectedProvince, setValue]);
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-		console.log(formData);
+    console.log(formData);
 
-		axios.post("http://localhost:3000/blood-drive/create", {
-			institute: formData.institute,
-			province: selectedProvince?.ProvinceID,
-			designation: formData.designation,
-			schedule: formData.schedule
-		})
-			.then((response) => {
-				console.log("success");
-				console.log(response.data);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
+    axios
+      .post("http://localhost:3000/blood-drive/create", {
+        institute: formData.institute,
+        province: selectedProvince?.ProvinceID,
+        designation: formData.designation,
+        schedule: formData.schedule,
+      })
+      .then((response) => {
+        console.log("success");
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-		newUsersInsertRequest(formData, "host-blood-drive");
+    newUsersInsertRequest(formData, "host-blood-drive");
 
-		setFormData({
-			institute: "",
-			province:"",
-			designation: "",
-			schedule: ""
-		});
-	};
+    setFormData({
+      institute: "",
+      province: "",
+      designation: "",
+      schedule: "",
+    });
+  };
 
-	const HostBloodDrivePageDetails = {
-		quote: {
-			classHint: "quote host-drive-quote",
-			quoteText: `“Host a blood drive and witness the power of community. Every drop tells a story of hope, each donation a narrative of compassion. Together, we can paint a masterpiece of aid—where the art is survival, and the medium is kindness.”`,
-		},
-		benefits_host_drive: {
-			subheadingText: "Being a Hero",
-			headingText: "Benefits of Hosting a Blood Drive",
-			classHint: "side-col-image benefits-host-drive",
-			paraText: `Hosting a blood drive is a great way to give back to your community and help save lives.
+  const HostBloodDrivePageDetails = {
+    quote: {
+      classHint: "quote host-drive-quote",
+      quoteText: `“Host a blood drive and witness the power of community. Every drop tells a story of hope, each donation a narrative of compassion. Together, we can paint a masterpiece of aid—where the art is survival, and the medium is kindness.”`,
+    },
+    benefits_host_drive: {
+      subheadingText: "Being a Hero",
+      headingText: "Benefits of Hosting a Blood Drive",
+      classHint: "side-col-image benefits-host-drive",
+      paraText: `Hosting a blood drive is a great way to give back to your community and help save lives.
 			By providing a convenient location for people to donate, you can help ensure that there is a steady supply of blood for those in need.
 			Blood drives also provide an opportunity for team building and community involvement, and can boost morale and engagement among employees or group members.`,
-			imageUrl: "../../../assets/images/blood-donation(1).jpg",
-		},
+      imageUrl: "../../../assets/images/blood-donation(1).jpg",
+    },
 
-		hosting_blood_drive: {
-			subheadingText: "",
-			headingText: "Hosting the Blood Drive",
-			classHint: "side-col-image hosting-blood-drive",
-			paraText: `On the day of the blood drive, it's important to ensure that everything runs smoothly and that donors have a positive experience.
+    hosting_blood_drive: {
+      subheadingText: "",
+      headingText: "Hosting the Blood Drive",
+      classHint: "side-col-image hosting-blood-drive",
+      paraText: `On the day of the blood drive, it's important to ensure that everything runs smoothly and that donors have a positive experience.
 			Make sure you have enough volunteers to help with registration, refreshments, and donor follow-up.
 			Provide a comfortable and welcoming environment for donors, and ensure that all equipment is properly sanitized and maintained.`,
-			imageUrl: "../../../assets/images/blood-donation(1).jpg",
-		},
+      imageUrl: "../../../assets/images/blood-donation(1).jpg",
+    },
 
-		hero: {
-			subheadingText: "Join Us to Save Lives",
-			headingText: "Host a Blood Drive to Save Lives With Us",
-			classHint: "host-blood-drive-page-hero",
-		},
-		stepsText: {
-			subheadingText: "Guide for Hosting",
-			headingText: "Promoting Your Blood Drive",
-		},
-	};
+    hero: {
+      subheadingText: "Join Us to Save Lives",
+      headingText: "Host a Blood Drive to Save Lives With Us",
+      classHint: "host-blood-drive-page-hero",
+    },
+    stepsText: {
+      subheadingText: "Guide for Hosting",
+      headingText: "Promoting Your Blood Drive",
+    },
+  };
 
-	const stepDetails = [
-		{
-			key: "promote-widely",
-			stepNumber: "01",
-			stepName: "Promote Widely",
-			stepDescription:
-				"Use social media, flyers, and emails to spread the word.",
-		},
-		{
-			key: "emphasize-benefits",
-			stepNumber: "02",
-			stepName: "Emphasize Benefits",
-			stepDescription: "Highlight the positive impact donors can make.",
-		},
-		{
-			key: "varity-of-channels",
-			stepNumber: "03",
-			stepName: "Variety of Channels",
-			stepDescription:
-				"Use multiple marketing channels to reach potential donors.",
-		},
-	];
+  const stepDetails = [
+    {
+      key: "promote-widely",
+      stepNumber: "01",
+      stepName: "Promote Widely",
+      stepDescription:
+        "Use social media, flyers, and emails to spread the word.",
+    },
+    {
+      key: "emphasize-benefits",
+      stepNumber: "02",
+      stepName: "Emphasize Benefits",
+      stepDescription: "Highlight the positive impact donors can make.",
+    },
+    {
+      key: "varity-of-channels",
+      stepNumber: "03",
+      stepName: "Variety of Channels",
+      stepDescription:
+        "Use multiple marketing channels to reach potential donors.",
+    },
+  ];
 
-	const fields = [
-		{
-			key: "institute",
-			name: "institute",
-			type: "text",
-			placeholder: "Institute",
-			required: true,
-		},
-		{
-			key: "province",
-			name: "province",
-			type: "select",
-			options: provinces.map((province) => ({
-			value: province.ProvinceID,
-			label: province.Name
-			})),
-			placeholder: "Province",
-			required: true,
-		},
-		{
-			key: "designation",
-			name: "designation",
-			type: "text",
-			placeholder: "Designation",
-			required: false,
-		},
-		{
-			key: "schedule",
-			name: "schedule",
-			type: "date",
-			placeholder: "Schedule",
-			required: true,
-		},
-	];
+  const fields = [
+    {
+      key: "institute",
+      name: "institute",
+      type: "text",
+      placeholder: "Institute",
+      required: true,
+    },
+    {
+      key: "province",
+      name: "province",
+      type: "select",
+      options: provinces.map((province) => ({
+        value: province.ProvinceID,
+        label: province.Name,
+      })),
+      placeholder: "Province",
+      required: true,
+    },
+    {
+      key: "designation",
+      name: "designation",
+      type: "text",
+      placeholder: "Designation",
+      required: false,
+    },
+    {
+      key: "schedule",
+      name: "schedule",
+      type: "date",
+      placeholder: "Schedule",
+      required: true,
+    },
+  ];
 
-	return (
-		<>
-			<HeaderComponent />
-			<HeroComponent {...HostBloodDrivePageDetails.hero} />
-			<FormComponent
-				fields={fields}
-				heading={"Host a Blood Drive"}
-				buttonText={"Schedule Host"}
-				handleSubmit={handleSubmit}
-				formData={formData}
-				setFormData={setFormData}
-			/>
-			<ThreeStepProcessComponent
-				stepsText={HostBloodDrivePageDetails.stepsText}
-				stepDetails={stepDetails}
-			/>
-			<SideBySideComponent
-				{...HostBloodDrivePageDetails.benefits_host_drive}
-			/>
+  return (
+    <>
+      <HeaderComponent />
+      <HeroComponent {...HostBloodDrivePageDetails.hero} />
+      <FormComponent
+        fields={fields}
+        heading={"Host a Blood Drive"}
+        buttonText={"Schedule Host"}
+        handleSubmit={handleSubmit}
+        formData={formData}
+        setFormData={setFormData}
+      />
+      <ThreeStepProcessComponent
+        stepsText={HostBloodDrivePageDetails.stepsText}
+        stepDetails={stepDetails}
+      />
+      <SideBySideComponent {...HostBloodDrivePageDetails.benefits_host_drive} />
 
-			<QuoteComponent {...HostBloodDrivePageDetails.quote} />
-			<SideBySideComponent
-				{...HostBloodDrivePageDetails.hosting_blood_drive}
-			/>
-			<BeforeFooterCTA />
-			<FooterComponent />
-		</>
-	);
+      <QuoteComponent {...HostBloodDrivePageDetails.quote} />
+      <SideBySideComponent {...HostBloodDrivePageDetails.hosting_blood_drive} />
+      <BeforeFooterCTA />
+      <FooterComponent />
+    </>
+  );
 };
 
 export default HostBloodDrivePage;
