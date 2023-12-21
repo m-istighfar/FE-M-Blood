30c8526a232968582a8a30033f1f978ebeb660db
m-istighfar <istighfar.amal@gmail.com>
2023-12-22 01:05:42 +0700

    handling form - need help

--- src/components/pages/contact/contact-page.jsx
+++ src/components/pages/contact/contact-page.jsx
@@ -1,21 +1,27 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { Modal } from "flowbite-react";
 import HeroComponent from "../../sections/hero/hero-component";
 import FormComponent from "../../sections/form/form-component";
-import ContactDetailsComponent from "../../sections/details/details-component";
 import HeaderComponent from "../../sections/header/header-component";
 import BeforeFooterCTA from "../../sections/before-footer-cta/before-footer-cta-components";
 import FooterComponent from "../../sections/footer/footer-component";
-import { useForm } from "react-hook-form";
+import ContactDetailsComponent from "../../sections/details/details-component";
 
-import axios from "axios";
 import { FaPhoneAlt, FaMapMarkerAlt } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
 const ContactPage = () => {
-  const [formStatus, setFormStatus] = useState({ loading: false, message: "" });
   const [bloodTypes, setBloodTypes] = useState([]);
-  const [provinces, setProvinces] = useState([]);
 
+  const [provinces, setProvinces] = useState([]);
+  const [modalContent, setModalContent] = useState({
+    isVisible: false,
+    title: "",
+    body: "",
+  });
   const {
     register,
     handleSubmit,
@@ -24,61 +30,76 @@ const ContactPage = () => {
   } = useForm();
 
   useEffect(() => {
-    const fetchBloodTypes = async () => {
-      try {
-        const response = await axios.get("http://localhost:3000/blood-type");
-        setBloodTypes(response.data.data);
-      } catch (error) {
-        console.error("Error fetching blood types:", error);
-      }
-    };
-    const fetchProvinces = async () => {
-      try {
-        const response = await axios.get("http://localhost:3000/province");
-        setProvinces(response.data.data);
-      } catch (error) {
-        console.error("Error fetching provinces:", error);
-      }
-    };
-
-    fetchBloodTypes();
-    fetchProvinces();
+    fetchData();
   }, []);
 
+  const fetchData = async () => {
+    await fetchBloodTypes();
+    await fetchProvinces();
+  };
+
+  const fetchBloodTypes = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/blood-type");
+      setBloodTypes(response.data.data);
+    } catch (error) {
+      console.error("Error fetching blood types:", error);
+    }
+  };
+
+  const fetchProvinces = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/province");
+      setProvinces(response.data.data);
+    } catch (error) {
+      console.error("Error fetching provinces:", error);
+    }
+  };
+
   const onSubmit = async (data) => {
-    setFormStatus({ loading: true, message: "" });
-    const accessToken = localStorage.getItem("accessToken");
-    if (!accessToken) {
-      console.error("No access token found. User must be logged in.");
-      setFormStatus({ loading: false, message: "User must be logged in." });
+    if (!localStorage.getItem("accessToken")) {
+      showModal("Error", "User must be logged in.");
       return;
     }
 
     data.isWillingToDonate = data.isWillingToDonate === "true";
     data.canHelpInEmergency = data.canHelpInEmergency === "true";
     try {
-      const response = await axios.post(
-        "http://localhost:3000/help-offer/offer",
-        data,
-        {
-          headers: { Authorization: `Bearer ${accessToken}` },
-        }
-      );
-      console.log("Help offer created successfully:", response.data);
-      reset();
-      setFormStatus({
-        loading: false,
-        message:
-          "Help offer created successfully. Thank you for your contribution!",
-      });
+      await submitHelpOffer(data);
     } catch (error) {
-      console.error("Error creating help offer:", error);
-      let errorMessage = "Error creating help offer.";
-      if (error.response && error.response.data && error.response.data.error) {
-        errorMessage = error.response.data.error;
-      }
-      setFormStatus({ loading: false, message: errorMessage });
+      handleSubmissionError(error);
     }
+  };
+
+  const submitHelpOffer = async (data) => {
+    const accessToken = localStorage.getItem("accessToken");
+    const response = await axios.post(
+      "http://localhost:3000/help-offer/offer",
+      data,
+      { headers: { Authorization: `Bearer ${accessToken}` } }
+    );
+    console.log("Help offer created successfully:", response.data);
+    reset();
+    showModal(
+      "Success",
+      "Help offer created successfully. Thank you for your contribution!"
+    );
+  };
+
+  const handleSubmissionError = (error) => {
+    let errorMessage = "Error creating help offer.";
+    if (error.response?.data?.error) {
+      errorMessage = error.response.data.error;
+    }
+    showModal("Error", errorMessage);
+  };
+
+  const showModal = (title, body) => {
+    setModalContent({ isVisible: true, title, body });
+  };
+
+  const closeModal = () => {
+    setModalContent({ ...modalContent, isVisible: false });
   };
 
   const ContactPageDetails = {
@@ -175,7 +196,7 @@ const ContactPage = () => {
       <HeaderComponent />
       <HeroComponent {...ContactPageDetails.hero} />
       <FormComponent
-        isLoading={formStatus.loading}
+        isLoading={modalContent.isVisible} // Use modalContent.isVisible for loading status
         fields={fields}
         register={register}
         handleSubmit={handleSubmit(onSubmit)}
@@ -183,8 +204,13 @@ const ContactPage = () => {
         heading={"Want to Help? Let Us Know"}
         buttonText={"Submit"}
       />
-      {formStatus.message && (
-        <p className="error-message text-center">{formStatus.message}</p>
+      {modalContent.isVisible && (
+        <Modal show={modalContent.isVisible} onClose={closeModal}>
+          <Modal.Header>{modalContent.title}</Modal.Header>
+          <Modal.Body>
+            <p>{modalContent.body}</p>
+          </Modal.Body>
+        </Modal>
       )}
       <ContactDetailsComponent contactDetails={contactDetails} />
       <BeforeFooterCTA />
